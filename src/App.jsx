426d057de3f59de4a81cc1fcf377d73967ea9421
devmariano <developer.mariano@gmail.com>
2023-06-18 20:41:28 -0300

    [M2S06] EX03 - Criação do componente Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+
 import Header from './components/Header/Header'
+import Banner from './components/Banner/Banner'
 
 function App() {
 
   return (
     <>
     <Header/>
+    <Banner texto="Os melhores cursos DEV"/>
     <h1>Hello word</h1>
 
     </>
